9a0e28c49d5ded5e90bcef387c41467fbcd1a7b7
Chang-Yu Chang <changyuchang5@gmail.com>
2026-03-31 08:30:01 +0800

    fixate IP host and port

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -6,7 +6,8 @@ options(shiny.autoreload = TRUE)
 options(shiny.autoload.js.version = TRUE)
 
 # Comment this if you don't want the app to be served on a random port
-options(shiny.port = httpuv::randomPort())
+options(shiny.host = '0.0.0.0') 
+options(shiny.port = 1234) # A standard port for Shiny
 
 # Print development instructions
 cat("\n")
